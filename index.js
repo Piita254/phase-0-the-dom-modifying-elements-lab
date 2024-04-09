e541b3bd19c9c02b29dfb2f6f25723a84092b2bb
Peter Ndungu <petersonndungu995@gmail.com>
2024-04-09 14:02:25 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Write your code here!
+ // Remove the main element with id 'main'
+document.getElementById("main").remove();
+
+// Define the 'newHeader' variable and create a new <h1> node
+const newHeader = document.createElement('h1');
+
+// Set the id of the 'newHeader' variable to 'victory'
+newHeader.id = 'victory';
+
+// Set the text content of the 'newHeader' variable
+newHeader.textContent = "YOUR-NAME is the champion"; // Replace YOUR-NAME with your actual name
+
+// Append the 'newHeader' to the body of the document
+document.body.appendChild(newHeader);
